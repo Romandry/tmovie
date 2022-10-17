863bdfd9d011a91a6455558dc9091c4780b74e5e
Andriy <transkyy@gmail.com>
2022-10-17 15:35:03 +0300

    Movie Controller, Model, Migration

---
+++ database/migrations/2022_10_17_095314_create_movies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigInteger('id_movie', true);
+            $table->string('name_movie')->comment('Название фильма');
+            $table->string('cover_url')->comment('URL превью');
+            $table->string('video')->comment('URL фильма');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
